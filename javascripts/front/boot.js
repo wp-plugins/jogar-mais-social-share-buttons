19ccfd8cbfc275137e808f3f111a8ffc33ea9e9d
victorfreitas1 <victorfreitas1@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-11-22 19:56:03 +0000

    Tag 2.0.0
    git-svn-id: https://plugins.svn.wordpress.org/jogar-mais-social-share-buttons/tags/2.0.0@1292134 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ javascripts/front/boot.js
@@ -0,0 +1,17 @@
+jQuery(function($) {
+
+	'use strict';
+
+	var container = $( 'body' )
+	  , component = container.byComponent( window.ssbPrefix )
+	;
+
+	if ( !component.length ) {
+		return;
+	}
+
+	new Application( container );
+	new CounterSocialShare( component );
+	new isMobile( component );
+	new hideElements( component );
+});
